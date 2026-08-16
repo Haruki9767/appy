8998dab287d926097d8728f4642f73f0b95c3324
Haruki <satishkalita526@gmail.com>
2026-08-16 22:28:36 +0530

    Create project.dart

---
+++ lib/models/project.dart
@@ -0,0 +1,55 @@
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'project.g.dart';
+
+@HiveType(typeId: 2)
+class Project {
+  @HiveField(0)
+  final String id;
+  
+  @HiveField(1)
+  String title;
+  
+  @HiveField(2)
+  String? description;
+  
+  @HiveField(3)
+  String? subject;
+  
+  @HiveField(4)
+  String priority;
+  
+  @HiveField(5)
+  String status;
+  
+  @HiveField(6)
+  String? deadline;
+  
+  @HiveField(7)
+  int progress;
+  
+  @HiveField(8)
+  String? notes;
+  
+  @HiveField(9)
+  String type;
+  
+  @HiveField(10)
+  String createdAt;
+
+  Project({
+    String? id,
+    required this.title,
+    this.description,
+    this.subject,
+    this.priority = 'medium',
+    this.status = 'notstarted',
+    this.deadline,
+    this.progress = 0,
+    this.notes,
+    required this.type,
+    String? createdAt,
+  })  : id = id ?? const Uuid().v4(),
+        createdAt = createdAt ?? DateTime.now().toIso8601String();
+}
