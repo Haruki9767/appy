e507513c7130d0a0e88a47ed576f387151f435a2
Haruki <satishkalita526@gmail.com>
2026-08-16 23:43:35 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.l1mefocus.app"
+    namespace = "com.example.flutter_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"  // ✅ Fixed: ndkVersion set correctly
 
